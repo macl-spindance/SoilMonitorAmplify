cf72d388568a4b2ebeb33d91f1a4528d4bdac77c
Mac Lobdell <mac.lobdell@spindance.com>
2021-07-26 18:54:59 -0500

    Initial Commit

---
+++ SoilMonitorAmplify/SoilMonitorAmplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoilMonitorAmplifyApp.swift
+//  SoilMonitorAmplify
+//
+//  Created by Mac Lobdell on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SoilMonitorAmplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
